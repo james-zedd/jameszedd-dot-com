060ddd5a1142a7379d2089fb1e0d49d3ceaf33b1
James Zedd <james.zedd@gmail.com>
2026-07-19 22:21:45 -0400

    Add more quotes

--- src/app/_data/Quotes.ts
+++ src/app/_data/Quotes.ts
@@ -9,7 +9,31 @@ export const quotes: Quote[] = [
         author: "Steve Jobs",
     },
     {
-        quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
-        author: "Winston Churchill"
+        quote: "No amount of money can buy the peace of mind that comes with finding a path you want to stay on.",
+        author: "Paul Millerd"
+    },
+    {
+        quote: "Do something that you are afraid of every day.",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        quote: "Dream as if you'll live forever. Live as if you'll die today.",
+        author: "James Dean"
+    },
+    {
+        quote: "No matter how much wealth we gain, how much knowledge we acquire, how much fame we win, unless we have loved strongly, warmly, unselfishly, and have been loved again in the same way, we have failed.",
+        author: "Pastor Ross Perry"
+    },
+    {
+        quote: "In Iaido we train to control a sword and in so doing learn to control ourselves.",
+        author: "Unknown"
+    },
+    {
+        quote: "If you want to be a lion, train with lions.",
+        author: "Carlson Gracie"
+    },
+    {
+        quote: "A black belt is a white belt that refused to give up.",
+        author: "Unknown / Attributed to many"
     }
 ];
